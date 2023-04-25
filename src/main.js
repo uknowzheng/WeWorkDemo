3a729fbff4970984cbee4c58caf27bccea32f07c
Xiaoqian Zheng <xiaoqian.zheng@shopee.com>
2023-04-25 15:45:15 +0800

    feat: update ui 库

--- src/main.js
+++ src/main.js
@@ -1,39 +1,42 @@
-import Vue from 'vue'
-import App from './App'
-import router from './router'
-import config from '@/config'
-import store from './store/index.js'
-import cdire from './libs/directive/index'
-import '@/assets/icons/iconfont.css'
+import Vue from "vue";
+import App from "./App";
+import router from "./router";
+import config from "@/config";
+import store from "./store/index.js";
+import cdire from "./libs/directive/index";
+import VueMaterial from "vue-material";
+import "@/assets/icons/iconfont.css";
+import "vue-material/dist/vue-material.min.css";
+import "vue-material/dist/theme/default.css";
 
 //自定义全局组件
-import CusConfirm from '@/components/other/confirm/CusConfirm'
+import CusConfirm from "@/components/other/confirm/CusConfirm";
 Vue.prototype.$confirm = CusConfirm.install;
 
-
-import Message from '@/components/other/message'
+import Message from "@/components/other/message";
 Vue.prototype.$message = Message.install;
 
 /**
  * @description 全局注册应用配置
  */
-Vue.prototype.$config = config
+Vue.prototype.$config = config;
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.use(VueMaterial);
 
 // 拖拽命令
 const directive = {
-  'drag': cdire.drag,
-}
+  drag: cdire.drag,
+};
 
-Object.keys(directive).forEach(key => {
-  Vue.directive(key, directive[key])
-})
-
+Object.keys(directive).forEach((key) => {
+  Vue.directive(key, directive[key]);
+});
 
 new Vue({
-  el: '#app',
+  el: "#app",
   router,
   store,
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
