f24848e9ab4c385871266935feb3edd187143f51
Yuki菌 <853294317@qq.com>
2025-05-21 17:13:17 +0800

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -1,9 +1,8 @@
 module.exports = {
-  // 本地开发的时候vue cli proxy代理的地址localhost ----- 118.31.43.13:8008
-  devHost: "http://118.31.43.13:8008",
+  // 本地开发的时候vue cli proxy代理的地址localhost
+  devHost: "http://localhost:8008",
   // 这个是实际代码运行时候，ws连接的地址
   wsHost: "ws://localhost",
-  // wsHost: "ws://118.31.43.13:8008",
   // 这块配置暂时没用到
   publicPath: {
     //  本地环境发布目录
